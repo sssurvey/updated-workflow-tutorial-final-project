f9b7034c46443a7632d3ee9b7846ad486643f513
Haomin Shi <haominshi@gmail.com>
2023-09-05 12:58:06 -0400

    added: sample data for todos

--- app/src/main/java/com/haomins/workflowtutorialfinal/model/TodoModel.kt
+++ app/src/main/java/com/haomins/workflowtutorialfinal/model/TodoModel.kt
@@ -2,5 +2,5 @@ package com.haomins.workflowtutorialfinal.model
 
 data class TodoModel(
     val title: String,
-    val content: String
+    val note: String
 )
--- app/src/main/java/com/haomins/workflowtutorialfinal/workflows/TodoListWorkflow.kt
+++ app/src/main/java/com/haomins/workflowtutorialfinal/workflows/TodoListWorkflow.kt
@@ -1,17 +1,27 @@
 package com.haomins.workflowtutorialfinal.workflows
 
+import com.haomins.workflowtutorialfinal.model.TodoModel
 import com.haomins.workflowtutorialfinal.screens.TodoListScreen
 import com.squareup.workflow1.Snapshot
 import com.squareup.workflow1.StatefulWorkflow
 
-object TodoListWorkflow : StatefulWorkflow<Unit, TodoListWorkflow.State, Nothing, TodoListScreen>() {
+object TodoListWorkflow :
+    StatefulWorkflow<Unit, TodoListWorkflow.State, Nothing, TodoListScreen>() {
 
     data class State(
-        val placeholder: String = ""
+        val todos: List<TodoModel>
     )
 
     override fun initialState(props: Unit, snapshot: Snapshot?): State {
-        return State("initial")
+        return State(
+            todos = listOf(
+                TodoModel(
+                    title = "Take the cat for a walk",
+                    note = "Cats really need their outside sunshine time. Don't forget to walk " +
+                            "Charlie. Hamilton is less excited about the prospect."
+                )
+            )
+        )
     }
 
     override fun render(
@@ -21,7 +31,7 @@ object TodoListWorkflow : StatefulWorkflow<Unit, TodoListWorkflow.State, Nothing
     ): TodoListScreen {
         return TodoListScreen(
             username = "",
-            todoTitles = emptyList(),
+            todoTitles = renderState.todos.map { it.title },
             onTodoSelected = {},
             onBack = {}
         )
